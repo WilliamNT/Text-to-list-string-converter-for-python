cc5a2209c83ad3c2d52c9183e90927f2795ed1f1
William <71773977+WilliamNT@users.noreply.github.com>
2021-06-07 18:18:54 +0200

    Added comments to the code for intelligibility

--- ListStringConverter.py
+++ ListStringConverter.py
@@ -1,14 +1,17 @@
 import re
 import string
 
-strName = input("What should we call this string? > ")
-inputText = input("Please type or paste your text here you wish to convert to a list string. > ")
+# Asking for string name and our base text
+strName = input("What should we call this string? > ").lower()
+inputText = input("Please type or paste your text here you wish to convert to a list string. > ").lower()
 
+# Converting our data to a list string
 convertedString = re.sub('['+string.punctuation+']', '', inputText).split()
 a = set(convertedString)
 b = list(a)
 outputString = strName + " = " + str(b)
 
+# Generating a file with the name of the string, then creating it's content
 with open(strName + ".txt", 'w') as outputTxt:
     outputTxt.write(outputString)
 print("Your output is:", outputString)
